a89cab7be77dfc2bdaf9853f3a3e3f429267d718
Shrikant-Sharat <shrikantsharat.k@gmail.com>
2011-09-01 11:34:37 +0530

    Simplified core module and moved filename construction logic to recorder

--- src/screenshot_tracker/core.clj
+++ src/screenshot_tracker/core.clj
@@ -1,44 +1,15 @@
 (ns screenshot-tracker.core
   (:require [screenshot-tracker.recorders :as recorders]
             [screenshot-tracker.wm-utils :as wm-utils]
-            [screenshot-tracker.gui :as gui])
-  (:require [clj-time.core :as ctime]))
-
-(def *filename-format* "shots/shot-{month}-{date}-{hour}-{minute}-{second}")
-
-(defn format-str
-  [template legend]
-  (reduce (fn [template [key value]] (.replace template
-                                               (str "{" (name key) "}")
-                                               (str value)))
-          template
-          (seq legend)))
-
-(defn get-filename-format-data
-  []
-  (let [now (ctime/now)]
-    {:year    (ctime/year now)
-     :month   (ctime/month now)
-     :date    (ctime/day now)
-     :day     (ctime/day-of-week now)
-     :hour    (ctime/hour now)
-     :minute  (ctime/minute now)
-     :second  (ctime/sec now)}))
-
-(defn construct-filename
-  []
-  (format-str *filename-format* (get-filename-format-data)))
+            [screenshot-tracker.gui :as gui]))
 
 (defn shoot-every
   "Save scrites every `time-gap` seconds, by calling the `save-fn` to save the scrite"
   [channel time-gap save-fn]
   (if (@channel :shoot?)
-    (let [filename (construct-filename)]
-      (save-fn
-        {:title (wm-utils/get-active-window-title)
-         :img filename}
-        (wm-utils/get-screenshot-data))
-      (println "Saved shot " filename)))
+    (save-fn
+      {:title (wm-utils/get-active-window-title)}
+      (wm-utils/get-screenshot-data)))
   (Thread/sleep (long (* time-gap 1000)))
   (recur channel time-gap save-fn))
 
@@ -49,23 +20,16 @@
       :on-start (fn [e]
                   (if (nil? (@channel :shoot?))
                     (.start (Thread.
-                              (fn [] (shoot-every channel 5 (recorders/get-sql-recorder "scrite.db"))))))
+                              (fn [] (shoot-every
+                                       channel
+                                       5
+                                       (recorders/get-sql-recorder
+                                         "scrite.db"
+                                         "shots/shot-{month}-{date}-{hour}-{minute}-{second}"))))))
                   (swap! channel assoc :shoot? true))
       :on-stop (fn [e]
                  (swap! channel assoc :shoot? false)))))
 
 (defn -main
   []
-  (comment save-shot "shot.png")
-  (comment save-shot (construct-filename))
-  (comment println (get-active-window-title))
-  (comment println (get-active-window-pid))
-  (comment println (get-active-window-cmd))
-  (comment (recorders/get-sql-recorder "scrite.db")
-     {:title (wm-utils/get-active-window-title)
-      :cmd (wm-utils/get-active-window-cmd)}
-     (wm-utils/get-screenshot-data))
-  (comment shoot-every 5 (recorders/get-sql-recorder "scrite.db"))
-  (comment .start (Thread. (fn [] (shoot-every 5 (recorders/get-sql-recorder "scrite.db")))))
-  (comment gui/show-main)
   (start-shooting))
--- src/screenshot_tracker/recorders.clj
+++ src/screenshot_tracker/recorders.clj
@@ -1,18 +1,41 @@
 (ns screenshot-tracker.recorders
   (:import [java.sql DriverManager]
            [java.io File]
-           [javax.imageio ImageIO]))
+           [javax.imageio ImageIO])
+  (:require [clj-time.core :as ctime]))
+
+(defn format-str
+  [template legend]
+  (reduce (fn [template [key value]] (.replace template
+                                               (str "{" (name key) "}")
+                                               (str value)))
+          template
+          (seq legend)))
+
+(defn construct-filename
+  [filename-format]
+  (let [now (ctime/now)
+        format-data {:year    (ctime/year now)
+                     :month   (ctime/month now)
+                     :date    (ctime/day now)
+                     :day     (ctime/day-of-week now)
+                     :hour    (ctime/hour now)
+                     :minute  (ctime/minute now)
+                     :second  (ctime/sec now)}]
+    (format-str filename-format format-data)))
 
 (defn get-sql-recorder
-  [db-path]
+  [db-path filename-format]
   (Class/forName "org.sqlite.JDBC")
   (let [db-con (DriverManager/getConnection "jdbc:sqlite:scrite.db")
         stmt (.prepareStatement
                db-con
                "INSERT INTO shots (title, img) VALUES (?, ?);")]
     (fn [item image-data]
-      (doto stmt
-        (.setString 1 (:title item))
-        (.setString 2 (:img item))
-        .execute)
-      (ImageIO/write image-data "png" (File. (str (:img item) ".png"))))))
+      (let [filename (str (construct-filename filename-format) ".png")]
+        (doto stmt
+          (.setString 1 (:title item))
+          (.setString 2 (:img item))
+          .execute)
+        (ImageIO/write image-data "png" (File. filename))
+        (println "Saved shot " filename)))))
