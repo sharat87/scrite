def55a2ba548c0a7b9ae1588c5c4417fd4330580
Shrikant-Sharat <shrikantsharat.k@gmail.com>
2011-09-05 12:55:58 +0530

    Added workaround for error with threading with latest leiningen.

--- src/scrite/core.clj
+++ src/scrite/core.clj
@@ -3,4 +3,7 @@
 
 (defn -main
   []
-  (start-shooting))
+  (start-shooting)
+  ; Following is required to workaround a bug in clojure+leiningen,
+  ; see http://stackoverflow.com/questions/7259072/clojure-java-shell-sh-throws-rejectedexecutionexception-when-run-in-a-new-thread/7293889#7293889
+  @(promise))
