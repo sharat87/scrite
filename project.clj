b89915fd3f7a9f4c49f4e1b1a44344ca89d6b469
Shrikant Sharat <shrikantsharat.k@gmail.com>
2012-04-25 21:33:34 +0530

    +jna dependencies.

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-time "0.4.1"]
                  [sqlitejdbc "0.5.6"]
-                 [seesaw "1.4.0"]]
+                 [seesaw "1.4.0"]
+                 [net.java.dev.jna/jna "3.4.0"]
+                 [net.java.dev.jna/platform "3.4.0"]]
   :main scrite.core)
