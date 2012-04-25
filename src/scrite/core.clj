bfece9b028b925bb9e0bdd6177c7304dcdf1a825
Shrikant Sharat <shrikantsharat.k@gmail.com>
2012-04-25 22:00:38 +0530

    Added a untested windows implementation.

--- src/scrite/core.clj
+++ src/scrite/core.clj
@@ -4,7 +4,8 @@
 
 ; Initialize OS-specific implementations of window manager functions.
 (require (case wm/current-os
-           :linux 'scrite.wm.linux))
+           :linux 'scrite.wm.linux
+           :windows 'scrite.wm.windows))
 
 (defn -main
   []
---
+++ src/scrite/wm/windows.clj
@@ -0,0 +1,32 @@
+(ns scrite.wm.windows
+  (:import com.sun.jna.Native
+           com.sun.jna.platform.win32.User32)
+  (:import [java.awt Rectangle Toolkit Robot])
+  (:use scrite.wm))
+
+(defn- active-window
+  []
+  (.GetForegroundWindow User32/INSTANCE))
+
+(defn- active-window-title
+  []
+  (let [title (char-array 1024)]
+    (.GetWindowText User32/INSTANCE (active-window) title 1024)
+    (Native/toString title)))
+
+(defn- active-window-class
+  []
+  (let [class-name (char-array 1024)]
+    (.GetClassName User32/INSTANCE (active-window) class-name 1024)
+    (Native/toString class-name)))
+
+(defmethod active-window-details :windows
+  []
+  {:title (active-window-title)
+   :class (active-window-class)})
+
+(defmethod get-screenshot-data :linux
+  []
+  (let [screen-rect (Rectangle. (.getScreenSize (Toolkit/getDefaultToolkit)))
+        capture (.createScreenCapture (Robot.) screen-rect)]
+    capture))
